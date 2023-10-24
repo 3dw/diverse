560bfe7085911e9f0806bb0913551831292b1182
Bestian Tang <bestian@gmail.com>
2023-10-24 12:42:07 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:bestian/diverse.git main:gh-pages
+git push -f git@github.com:3dw/diverse.git main:gh-pages
 
 cd -
